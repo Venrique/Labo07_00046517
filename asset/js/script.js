2b6d1135e153ab46a25950671fcb07c69b5025dc
venrique <00046517@uca.edu.sv>
2018-10-12 10:48:03 -0600

    se arregló una variable

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,9 +6,9 @@ container.style.width = "200px";
 container.style.height = "200px";
 container.innerHTML = "<input type='text' placeholder='escriba su texto'>";
 
-container = document.getElementsByClassName("c2");
+containers = document.getElementsByClassName("c2");
 
-for(let i of container){
+for(let i of containers){
     i.style.backgroundColor = "#009999"
     i.innerHTML = "<input type='text' placeholder='Escriba su texto'>";
 }
